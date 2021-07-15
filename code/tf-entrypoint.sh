134b34774b1743b1b2425596ed48f0f611c20453
Ubuntu <ubuntu@lucuma.maas>
2021-07-15 09:31:55 +0000

    updates

---
+++ code/tf-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+echo "Logging into maas server..."
+
+# MAAS_SERVER=10.245.128.4
+# MAAS_PORT=5240
+# MAAS_API_KEY="YQg7utpEbD5sZ5jyPP:frwtnUJbXReMtfZxtz:vnAAt9zrGZPxNKEGRyp76Eku5nedq2xD"
+
+# api key = final arg
+/usr/bin/maas login testflinger_a http://10.245.128.4:5240/MAAS/ \
+    YQg7utpEbD5sZ5jyPP:frwtnUJbXReMtfZxtz:vnAAt9zrGZPxNKEGRyp76Eku5nedq2xD
