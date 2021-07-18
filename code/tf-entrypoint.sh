f98a08dba7ccd80888c606d8a84b7128ea96b931
Adrian Lane <adrian.lane@canonical.com>
2021-07-18 01:54:04 -0700

    api update

--- code/tf-entrypoint.sh
+++ code/tf-entrypoint.sh
@@ -5,11 +5,18 @@ MAAS_SERVER=10.245.128.4
 MAAS_PORT=5240
 MAAS_API_KEY=(YQg7utpEbD5sZ5jyPP:frwtnUJbXReMtfZxtz:vnAAt9zrGZPxNKEGRyp76Eku5nedq2xD)
 
+echo
 echo "###########################"
+echo
 echo "Logging into maas server..."
-
 /usr/bin/maas login $TF_MAAS_ACT http://$MAAS_SERVER:$MAAS_PORT/MAAS/ $MAAS_API_KEY
 
+echo "Replicating maas-cli db..."
+cp /root/.maascli.db /home/ubuntu/
+cp /root/.maascli.db /home/testflinger/
+chown ubuntu:ubuntu /home/ubuntu/.maascli.db
+chown testflinger:testflinger /home/testflinger/.maascli.db
+echo
 echo "###########################"
 echo
 
