4393e468b9fcacff2b388163e7d79c16ff6d5f8b
Adrian Lane <adrian.lane@canonical.com>
2021-07-18 01:07:22 -0700

    entrypoint.sh

--- code/tf-entrypoint.sh
+++ code/tf-entrypoint.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/bash
 
+TF_MAAS_ACT=testflinger_a
+MAAS_SERVER=10.245.128.4
+MAAS_PORT=5240
+MAAS_API_KEY=(YQg7utpEbD5sZ5jyPP:frwtnUJbXReMtfZxtz:vnAAt9zrGZPxNKEGRyp76Eku5nedq2xD)
+
+echo "###########################"
 echo "Logging into maas server..."
 
-# MAAS_SERVER=10.245.128.4
-# MAAS_PORT=5240
-# MAAS_API_KEY="YQg7utpEbD5sZ5jyPP:frwtnUJbXReMtfZxtz:vnAAt9zrGZPxNKEGRyp76Eku5nedq2xD"
+/usr/bin/maas login $TF_MAAS_ACT http://$MAAS_SERVER:$MAAS_PORT/MAAS/ $MAAS_API_KEY
 
-# api key = final arg
-/usr/bin/maas login testflinger_a http://10.245.128.4:5240/MAAS/ \
-    YQg7utpEbD5sZ5jyPP:frwtnUJbXReMtfZxtz:vnAAt9zrGZPxNKEGRyp76Eku5nedq2xD
+echo "###########################"
+echo
 
 # start init
 /sbin/my_init -- bash -l
