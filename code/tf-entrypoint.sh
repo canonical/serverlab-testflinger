2e2c4068862713f953a133c79dc91e68e23fc59e
Adrian Lane <adrian.lane@canonical.com>
2021-07-18 00:43:25 -0700

    entrypoint.sh

--- code/tf-entrypoint.sh
+++ code/tf-entrypoint.sh
@@ -9,3 +9,6 @@ echo "Logging into maas server..."
 # api key = final arg
 /usr/bin/maas login testflinger_a http://10.245.128.4:5240/MAAS/ \
     YQg7utpEbD5sZ5jyPP:frwtnUJbXReMtfZxtz:vnAAt9zrGZPxNKEGRyp76Eku5nedq2xD
+
+# start init
+/sbin/my_init -- bash -l
