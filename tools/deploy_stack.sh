ad9a9fea89f76f19f072d2b14dcea6af849132aa
Adrian Lane <adrian.lane@canonical.com>
2021-07-21 23:05:02 -0700

    secret volume

--- tools/deploy_stack.sh
+++ tools/deploy_stack.sh
@@ -6,24 +6,23 @@ secrets_file=./.tfscr
 
 # docker-compose build does not support build secrets (in dev)
 # therefore, we must use discrete docker build cmds
-docker build -f testflinger-db \
-  -t redis \
-  # --progress=plain \
-  .
+docker build -t redis \
+  --progress=plain \
+  .  2>&1
 docker build -f testflinger-api \
   -t python:3.8-slim-buster \
-  # --progress=plain \
-  .
+  --progress=plain \
+  .  2>&1
 docker build -f testflinger-agent \
   -t phusion/baseimage:focal-1.0.0 \
   --secret id=tf_secret,src=$secrets_file \
-  # --progress=plain \
-  .
+  --progress=plain \
+  .  2>&1
 docker build -f testflinger-cli \
   -t phusion/baseimage:focal-1.0.0 \
   --secret id=tf_secret,src=$secrets_file \
-  # --progress=plain \
-  .
+  --progress=plain \
+  .  2>&1
 
 # start stack
 docker-compose start --detach 2>&1
