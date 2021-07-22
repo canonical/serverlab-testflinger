292e70a8b3d182b3074726b6ceecc58a2e6a36cc
Adrian Lane <adrian.lane@canonical.com>
2021-07-21 22:03:22 -0700

    secret volume

--- tools/deploy_stack.sh
+++ tools/deploy_stack.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
 
-COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose up --detach 2>&1
+export DOCKER_BUILDKIT=1
+
+docker build -f testflinger-db
+docker build -f testflinger-api
+docker build -f testflinger-agent \
+	--secret id=ubuntu_secret id=testflinger_secret \
+	--progress=plain \
+docker build -f testflinger-cli \
+	--secret id=ubuntu_secret id=testflinger_secret \
+	--progress=plain \
+docker-compose start --detach 2>&1
