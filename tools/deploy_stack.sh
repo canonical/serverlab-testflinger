aace0585739205961e78c1c48b434e3b5b05d9bf
Adrian Lane <adrian.lane@canonical.com>
2021-08-03 20:25:09 -0700

    updates

--- tools/deploy_stack.sh
+++ tools/deploy_stack.sh
@@ -2,6 +2,9 @@
 
 ubuntu_scr=(ch4ng3m3)
 testflinger_scr=(u24xeO6EKuWt)
+maas_api_key=(YQg7utpEbD5sZ5jyPP:frwtnUJbXReMtfZxtz:vnAAt9zrGZPxNKEGRyp76Eku5nedq2xD)
+maas_host=('10.245.128.4')
+maas_port=('5240')
 
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
@@ -9,6 +12,9 @@ export COMPOSE_DOCKER_CLI_BUILD=1
 docker-compose build \
   --build-arg ubuntu_scr=$ubuntu_scr \
   --build-arg testflinger_scr=$testflinger_scr \
+  --build-arg testflinger_scr=$maas_api_key \
+  --build-arg testflinger_scr=$maas_host \
+  --build-arg testflinger_scr=$maas_port \
   --progress=tty \
   --no-cache \
   --force-rm 2>&1
