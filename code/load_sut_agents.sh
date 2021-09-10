83b24359d15c606c74309cae96f219ae97b968fa
Adrian Lane <adrian.lane@canonical.com>
2021-09-10 02:43:19 -0700

    update docker-compose api cmd

--- code/load_sut_agents.sh
+++ code/load_sut_agents.sh
@@ -6,13 +6,20 @@ echo
 echo "Starting SUT agents:"
 echo
 
+mkdir /var/log/tf-agent
+
 for sutf in /data/testflinger-agent/sut/*; do
 	if [ -f "$sutf" ]; then
 		echo "$sutf"
-		testflinger-agent -c $sutf &
-		sleep 1.5
+		touch /var/log/tf-agent/${sutf%.*}
+		testflinger-agent -c $sutf & > /var/log/tf-agent/${sutf%.*} 2>&1
+		sleep .5
 	fi
 done
+
+for logf in /var/log/tf-agent/*; do
+	mv /var/log/tf-agent/$logf.conf
+
 
 echo
 echo "###########################"
