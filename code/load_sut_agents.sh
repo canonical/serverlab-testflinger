6e1cbdbd9f5b5092467c95d6b32c25290c858faf
Adrian Lane <adrian.lane@canonical.com>
2021-09-10 02:53:59 -0700

    update docker-compose api cmd

--- code/load_sut_agents.sh
+++ code/load_sut_agents.sh
@@ -9,7 +9,9 @@ echo
 # check if exists
 mkdir /var/log/tf-agent
 
-for sutf in /data/testflinger-agent/sut/*; do
+cd /data/testflinger-agent/sut
+
+for sutf in *; do
 	if [ -f "$sutf" ]; then
 		echo "$sutf"
 		touch /var/log/tf-agent/${sutf%.*}
