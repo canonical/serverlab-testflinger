72d60ba14f1f06b55bd2391f417b1086f7791f05
Adrian Lane <adrian.lane@canonical.com>
2021-09-12 02:09:22 -0700

    update docker-compose api cmd

--- code/tf-agent/20_hogplum.sh
+++ code/tf-agent/20_hogplum.sh
@@ -5,5 +5,5 @@ sut="hogplum"
 echo "* starting agent: $sut"
 touch /var/log/tf-agent/$sut
 
-nohup testflinger-agent -c /data/testflinger-agent/sut/$sut.conf &> /var/log/tf-agent/$sut
+testflinger-agent -c /data/testflinger-agent/sut/$sut.conf &> /var/log/tf-agent/$sut
 sleep .5
