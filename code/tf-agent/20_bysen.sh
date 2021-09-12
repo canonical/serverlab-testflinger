704ae93a35e908c490977f1a0f654d27e92cbab8
Adrian Lane <adrian.lane@canonical.com>
2021-09-12 01:05:42 -0700

    update docker-compose api cmd

--- code/tf-agent/20_bysen.sh
+++ code/tf-agent/20_bysen.sh
@@ -5,5 +5,5 @@ sut="bysen"
 echo "* starting agent: $sut"
 touch /var/log/tf-agent/$sut
 
-nohup testflinger-agent -c /data/testflinger-agent/sut/$sut &> /var/log/tf-agent/$sut
+nohup testflinger-agent -c /data/testflinger-agent/sut/$sut.conf &> /var/log/tf-agent/$sut
 sleep .5
