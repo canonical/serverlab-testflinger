927a4c7b7c82b35350418dbbfa6da616e8d4fb40
Adrian Lane <adrian.lane@canonical.com>
2021-09-12 03:06:01 -0700

    update docker-compose api cmd

--- code/tf-agent/20_garamond.sh
+++ code/tf-agent/20_garamond.sh
@@ -5,5 +5,5 @@ sut="garamond"
 echo "* starting agent: $sut"
 touch /var/log/tf-agent/$sut
 
-testflinger-agent -c /data/testflinger-agent/sut/$sut.conf &> /var/log/tf-agent/$sut
+testflinger-agent -c /data/testflinger-agent/sut/$sut.conf >> /var/log/tf-agent/$sut
 sleep .5
--- code/tf-agent/20_persianlime.sh
+++ code/tf-agent/20_persianlime.sh
@@ -5,5 +5,5 @@ sut="persianlime"
 echo "* starting agent: $sut"
 touch /var/log/tf-agent/$sut
 
-testflinger-agent -c /data/testflinger-agent/sut/$sut.conf &> /var/log/tf-agent/$sut
+testflinger-agent -c /data/testflinger-agent/sut/$sut.conf >> /var/log/tf-agent/$sut
 sleep .5
