92bd16036fa2d10f829f58ce896a465e407f32a4
Adrian Lane <adrian.lane@canonical.com>
2021-09-15 02:24:01 -0700

    subprocess wrapper for tf-agents

--- code/start_sut_agents.py
+++ code/start_sut_agents.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from os import path, listdir
+from os import path, listdir, setgid, setuid, setsid
 import subprocess
 import shlex
 # from pudb import set_trace; set_trace()
@@ -12,18 +12,31 @@ log_dir = path.join('/', 'var', 'log', 'sut-agent')
 print('~~~~~~~~~~~~~~~~~~~~~~~~~~~')
 print('Starting SUT agent(s):')
 
+
+def delegate(user_uid, user_gid):
+    "Execute as different user."
+    def preempt():
+        # daemonize
+        setsid()
+        # chuser
+        setgid(user_gid)
+        setuid(user_uid)
+    return preempt
+
+
 for sut_conf in listdir(conf_dir):
     sut = path.splitext(sut_conf)[0]
     log_path = path.join(log_dir, sut)
     conf_path = path.join(conf_dir, sut_conf)
     # easier to read
     cmd = shlex.split(
-        'nohup testflinger-agent -c %s' % conf_path)
+        'testflinger-agent -c %s' % conf_path)
 
     try:
         with open(log_path, 'w') as _file:
             agent = subprocess.Popen(
                 cmd,
+                preexec_fn=delegate(1000, 1000),  # run as ubuntu
                 shell=False,
                 start_new_session=True,
                 cwd=work_dir,
