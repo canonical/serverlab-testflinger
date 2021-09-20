a2b958fb568bc139dcc69acd7505e440256baa84
Adrian Lane <adrian.lane@canonical.com>
2021-09-20 13:44:27 -0700

    updated start_agent*

--- code/start_sut_agents.py
+++ code/start_sut_agents.py
@@ -2,7 +2,7 @@
 
 "Load specified SUT agents."
 
-from os import path, listdir, setgid, setuid, chmod
+import os
 import subprocess
 import shlex
 
@@ -10,31 +10,33 @@ import shlex
 def delegate(user_uid, user_gid):
     "Execute as different user."
     def preempt():
-        setgid(user_gid)
-        setuid(user_uid)
+        os.setgid(user_gid)
+        os.setuid(user_uid)
     return preempt
 
 
 def load_sut_agent(sut_conf, work_dir, conf_dir, log_dir):
     "Load specified SUT agent."
-    sut = path.splitext(sut_conf)[0]
+    sut = os.path.splitext(sut_conf)[0]
     # modify perms for logging
-    log_path = chmod((path.join(log_dir, sut)), 0o666)
-    conf_path = path.join(conf_dir, sut_conf)
+    log_path = os.path.join(log_dir, sut)
+    conf_path = os.path.join(conf_dir, sut_conf)
     # daemonize
     cmd = shlex.split(
         'setsid testflinger-agent -c %s' % conf_path)
 
     try:
-        with open(log_path, 'w') as _file:
-            _ = subprocess.Popen(  # pylint: disable=w1509
+        with open(os.open(
+                log_path, os.O_CREAT | os.O_WRONLY, 0o666), 'w') as logf:
+            # pylint: disable=w1509
+            _ = subprocess.Popen(
                 cmd,
                 preexec_fn=delegate(1000, 1000),  # run as
                 start_new_session=True,
                 universal_newlines=True,
                 encoding='utf-8',
                 cwd=work_dir,
-                stdout=_file,
+                stdout=logf,
                 stderr=subprocess.STDOUT)
     except OSError:
         print('  - Unable to start agent for: %s' % sut)
@@ -44,14 +46,14 @@ def load_sut_agent(sut_conf, work_dir, conf_dir, log_dir):
 
 def main():
     "Load specified SUT agents."
-    work_dir = path.join('/', 'data', 'testflinger-agent')
-    conf_dir = path.join(work_dir, 'sut')
-    log_dir = path.join('/', 'var', 'log', 'sut-agent')
+    work_dir = os.path.join('/', 'data', 'testflinger-agent')
+    conf_dir = os.path.join(work_dir, 'sut')
+    log_dir = os.path.join('/', 'var', 'log', 'sut-agent')
 
     print('~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     print('Starting SUT agent(s):')
 
-    for sut_conf in listdir(conf_dir):
+    for sut_conf in os.listdir(conf_dir):
         load_sut_agent(sut_conf, work_dir, conf_dir, log_dir)
 
 
