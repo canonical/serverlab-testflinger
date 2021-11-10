325f01e37b49030263fbe7609a9839bcfe3b0ac8
Adrian Lane <adrian.lane@canonical.com>
2021-11-09 21:23:50 -0800

    start sut agents

--- code/01_run_sut_agents.sh
+++ code/01_run_sut_agents.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
-exec /sbin/setuser ubuntu python3 /opt/start_sut_agents.py 2>&1
 
+exec /sbin/setuser ubuntu python3 /opt/start_sut_agents.py 2>&1
