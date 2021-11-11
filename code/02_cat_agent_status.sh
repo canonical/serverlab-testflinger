2447a5ce36e982459f8aa875393d70d4f6f35ddd
Adrian Lane <adrian.lane@canonical.com>
2021-11-11 02:48:01 -0800

    svc

--- code/02_cat_agent_status.sh
+++ code/02_cat_agent_status.sh
@@ -2,4 +2,4 @@
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 
-cat /var/log/sut-agent/init_agents
+# cat /var/log/sut-agent/init_agents
