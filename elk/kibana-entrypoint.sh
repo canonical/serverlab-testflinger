5a7aba80f03c7f1bcbdb2a668cc3b96e95773f50
Adrian Lane <adrian.lane@canonical.com>
2021-11-13 21:51:05 -0800

    kibana

--- elk/kibana-entrypoint.sh
+++ elk/kibana-entrypoint.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
-./bin/kibana-plugin install https://github.com/sivasamyk/logtrail/releases/download/v0.1.31/logtrail-7.9.2-0.1.31.zip
+./bin/kibana-plugin \
+install \
+https://github.com/sivasamyk/logtrail/releases/download/v0.1.31/logtrail-7.9.2-0.1.31.zip
