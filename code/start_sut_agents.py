4d613abab2aa062cfd583d8686cbbc658101bb6e
Adrian <z3r0@hubble.local>
2021-12-16 06:10:32 -0800

    start_sut_agents

--- code/start_sut_agents.py
+++ code/start_sut_agents.py
@@ -34,7 +34,7 @@ class LogAgent(threading.Thread):
         self.log_level = log_level
         self.pipe_logger = self.config_pipe_logging()
         self.start_time = time.time()
-        self.interval = 20  # lines
+        self.interval = 120  # lines
         time.sleep(3)
         self.loop_pipe()
 
