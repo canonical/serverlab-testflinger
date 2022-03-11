b18ff33f72d96740c150377206a6e5c6121f141b
Adrian <z3r0@hubble.local>
2022-03-11 05:09:53 -0800

    agnt_cntnr_entrypt

--- code/01_run_dckrd.sh
+++ code/01_run_dckrd.sh
@@ -7,21 +7,4 @@ env KILL_PROCESS_TIMEOUT=300
 # Kill all other processes (such as those which have been forked)
 env KILL_ALL_PROCESSES_TIMEOUT=300
 
-if ! pgrep containerd
-then
-    if ! pgrep dockerd
-    then
-        FILE=/var/run/docker.pid
-        if test -f "$FILE"
-        then
-            echo "$FILE exists; removing"
-            rm $FILE
-    fi
-
-    /usr/bin/dockerd --icc=true --log-level=info 2>&1 &
-    # /opt/start_dckrd.sh &
-    # wait for api to load
-    sleep 3
-else
-    echo '* dockerd running...'
-fi
+/usr/bin/dockerd --icc=true --log-level=info 2>&1 &
