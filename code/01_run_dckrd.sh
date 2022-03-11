f25e6d8a537f2f696fcad92effe7ab2a3a6bbd1d
Adrian <z3r0@hubble.local>
2022-03-11 05:17:31 -0800

    agnt_cntnr_entrypt

--- code/01_run_dckrd.sh
+++ code/01_run_dckrd.sh
@@ -7,4 +7,10 @@ env KILL_PROCESS_TIMEOUT=300
 # Kill all other processes (such as those which have been forked)
 env KILL_ALL_PROCESSES_TIMEOUT=300
 
+FILE=/var/run/docker.pid
+if test -f "$FILE"
+then
+    echo "$FILE exists; removing"
+    rm $FILE
+
 /usr/bin/dockerd --icc=true --log-level=info 2>&1 &
