21c025df68831946b3f996550a8e57f6c2a0d935
Adrian Lane <adrian.lane@canonical.com>
2023-04-25 18:33:36 -0700

    ensure influx env vars persist

--- tools/deploy_stack.sh
+++ tools/deploy_stack.sh
@@ -10,10 +10,18 @@ export COMPOSE_DOCKER_CLI_BUILD=1
 # temp pws
 UBUNTU_SCR=ch4ng3m3
 # TESTFLINGER_SCR=u24xeO6EKuWt
+INFLUX_HOST="10.245.128.9"
+INFLUX_PORT="8086"
+INFLUX_USER="root"
+INFLUX_PW="root"
 
 docker-compose build \
   --build-arg ubuntu_scr=$UBUNTU_SCR \
   --build-arg host_dir=$PWD \
+  --build-arg host_dir=$INFLUX_HOST \
+  --build-arg host_dir=$INFLUX_PW \
+  --build-arg host_dir=$INFLUX_USER \
+  --build-arg host_dir=$INFLUX_PORT \
   --progress=tty 2>&1
 
 # start stack
