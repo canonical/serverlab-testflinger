0b6f367269394f32a75b527e9ce0b335b8274dcd
Adrian Lane <adrian.lane@canonical.com>
2023-04-26 15:15:20 -0700

    mount influx env vars

--- code/02_config_influx.sh
+++ code/02_config_influx.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 # `/sbin/setuser memcache` runs the given command as the user `memcache`.
 # If you omit that part, the command will be run as root.
 
-source /opt/config_influx.sh 2>&1 &
+exec source /opt/config_influx.sh 2>&1 &
