f430d4a4276a818db428345ace9e4441677e0eab
Adrian Lane <adrian.lane@canonical.com>
2023-05-02 22:40:52 +0200

    modify orb nuke

--- tools/orb_nuke.sh
+++ tools/orb_nuke.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/bash
 
-docker stop $(docker ps -a -q)
+# docker stop $(docker ps -a -q)
 
-docker rm -vf $(docker ps -a -q)
+# docker rm -vf $(docker ps -a -q)
 
-docker rmi -f $(docker images -a -q)
+# docker rmi -f $(docker images -a -q)
 
-docker volume rm -f $(docker volume ls -q)
+# docker volume rm -f $(docker volume ls -q)
 
-docker network prune -f
+# docker network prune -f
+
+# docker builder prune -a -f
+
+
+docker stop $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
+
+docker rm -vf $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
+
+docker rmi -f $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
+
+docker volume rm -f $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
 
 docker builder prune -a -f
