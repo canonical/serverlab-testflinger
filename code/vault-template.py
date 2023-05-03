d4e8557d124632ff0512a56bcea3432b600d896b
Adrian Lane <adrian.lane@canonical.com>
2023-05-03 09:05:24 +0200

    add docker compose vault volume config

--- code/vault-template.py
+++ code/vault-template.py
@@ -1,11 +1,11 @@
 import hvac
 
 client = hvac.Client(
-    url='http://10.172.10.21:8200',
+    url='http://10.245.128.21:8200',
     token='nh-vault-root',
 )
 
 create_response = client.secrets.kv.v2.create_or_update_secret(
     path='influx',
-    secret=dict(host='10.172.10.17', port=8086, user='root', passw='root'),
+    secret=dict(host='10.245.128.17', port=8086, user='root', passw='root'),
 )
