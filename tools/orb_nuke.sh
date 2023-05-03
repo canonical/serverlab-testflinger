68c4f3dc9a4ce09d0381a9b901a90d30e12a23b9
Adrian Lane <adrian.lane@canonical.com>
2023-05-03 09:12:19 +0200

    add docker compose vault volume config

--- tools/orb_nuke.sh
+++ tools/orb_nuke.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/bash
 
-# docker stop $(docker ps -a -q)
+docker stop $(docker ps -a -q)
 
-# docker rm -vf $(docker ps -a -q)
+docker rm -vf $(docker ps -a -q)
 
-# docker rmi -f $(docker images -a -q)
+docker rmi -f $(docker images -a -q)
 
 # docker volume rm -f $(docker volume ls -q)
 
-# docker network prune -f
+docker volume rm -f $(docker ps -a --format '{{.Names}}' | grep -v 'vault_data' | xargs -I {} docker ps -q -f name={})
 
-# docker builder prune -a -f
-
-
-docker stop $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
-
-docker rm -vf $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
-
-docker rmi -f $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
-
-docker volume rm -f $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
+docker network prune -f
 
 docker builder prune -a -f
+
+
+# docker stop $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
+
+# docker rm -vf $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
+
+# docker rmi -f $(docker ps -a --format '{{.Names}}' | grep -v 'nh-vault' | xargs -I {} docker ps -q -f name={})
+
+# docker volume rm -f $(docker ps -a --format '{{.Names}}' | grep -v 'vault_data' | xargs -I {} docker ps -q -f name={})
+
+# docker builder prune -a -f
