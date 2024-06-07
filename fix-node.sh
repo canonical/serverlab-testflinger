c65834f792a3a54ab0a50bec7795993cbdc609cd
Jeff Lane <jeffrey.lane@canonical.com>
2024-06-07 14:51:45 -0400

    First few agents fixed to test the updates and fixes to TF after lab rebuild. fix-agent script added for now to help fix the remaining agents.

---
+++ fix-node.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NODE_NAME=$1
+NODE_ID=$2
+UPDATE_FILE_DIR=$PWD/UPDATES_NEEDED
+
+# copy files to PWD
+
+git mv $UPDATE_FILE_DIR/$NODE_NAME* $PWD/sut/
+
+# fix the node id in the file
+
+yq e ".node_id = \"$NODE_ID\"" -i ${PWD}/sut/${NODE_NAME}_snappy.yaml
+
+
