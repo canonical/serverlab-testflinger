df0f77e17d77c6a0681e7479efdb2e7feeb498e8
Jeff Lane <jeffrey.lane@canonical.com>
2024-06-12 12:14:42 -0400

    Updated first batch of agents. Added 3 new multi agents, and new agent for vera

--- fix-node.sh
+++ fix-node.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-NODE_NAME=$1
-NODE_ID=$2
+profile=$1
+NODE_NAME=$2
+NODE_ID=$3
 UPDATE_FILE_DIR=$PWD/UPDATES_NEEDED
 
+usage(){
+  echo "$0 maas_profile node_name node_id"
+}
+
+if [ "$#" -ne 3 ]; then
+  echo "Incorrect number of args passed"
+  echo
+  usage
+  exit 1
+  fi
 # copy files to PWD
 
-git mv $UPDATE_FILE_DIR/$NODE_NAME* $PWD/sut/
+if [[ -f "$PWD/sut/${NODE_NAME}.conf" && -f "$PWD/sut/${NODE_NAME}_snappy.conf" ]]; then
+  echo "Files already exist in $PWD/sut"
+else
+    echo "Copying files to $PWD/sut"
+    git mv $UPDATE_FILE_DIR/${NODE_NAME}* $PWD/sut/
+fi
 
 # fix the node id in the file
-
 yq e ".node_id = \"$NODE_ID\"" -i ${PWD}/sut/${NODE_NAME}_snappy.yaml
 
 # and make sure the maas user is updated
 yq e ".maas_user = \"testflinger-maastiff\"" -i ${PWD}/sut/${NODE_NAME}_snappy.yaml
 
+# verify maas connection and update storage if it works
+if maas $profile version read &> /dev/null; then
+    echo "MAAS connection verified, updating storage"
+    $PWD/code/capture_node_storage.py --profile $profile --clear $PWD/sut/${NODE_NAME}_snappy.yaml
+    $PWD/code/capture_node_storage.py --profile $profile --process $PWD/sut/${NODE_NAME}_snappy.yaml
+else
+    echo "MAAS connection failed, update storage later"
+fi
